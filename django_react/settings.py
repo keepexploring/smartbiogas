8a6ba55fab0d78c1e37fab21e2b476c3f706c030
Diego R. Herrera <diegorherrera@gmail.com>
2018-02-07 15:10:18 +0000

    Exclude suit

--- django_react/settings.py
+++ django_react/settings.py
@@ -40,7 +40,7 @@ LOGIN_REDIRECT_URL = 'home'
 INSTALLED_APPS = (
     'dal',
     'dal_select2',
-    'suit',
+    # 'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
