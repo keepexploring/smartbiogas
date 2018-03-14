c46cf7725e98f24dc9001e1d9fb7ee8cbeb50237
Joel <joel.c@scene.community>
2018-03-14 10:45:19 +0000

    admin updated

--- django_dashboard/admin.py
+++ django_dashboard/admin.py
@@ -13,6 +13,18 @@ from bootstrap_datepicker.widgets import DatePicker
 from datetimepicker.widgets import DateTimePicker
 from django_dashboard.forms import UserForm, BiogasForm, CompanyForm, UserDetailForm
 from django import forms
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
+
+class MyAdminSite(AdminSite):
+    # Text to put at the end of each page's <title>.
+    site_title = ugettext_lazy('SmartBiogas Admin')
+
+    # Text to put in each page's <h1> (and above login form).
+    site_header = ugettext_lazy('SmartBiogas Administration')
+
+    # Text to put at the top of the admin index page.
+    index_title = ugettext_lazy('SmartBiogas Administration')
 
 
 class TechnicianDetailAdmin(admin.StackedInline):
@@ -136,6 +148,8 @@ class UserAdmin(admin.ModelAdmin):
 
 
 #admin.ModelAdmin.ordering = ()
+#admin_site = MyAdminSite()
+admin.site.site_header = 'SmartBiogas Admin'
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Dashboard, DashboardAdmin)
 admin.site.register(UserDetail, UserDetailAdmin)
