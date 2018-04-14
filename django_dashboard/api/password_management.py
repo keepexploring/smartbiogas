bd974673dab0466d5f74f8bc17393661dca56f9d
Joel <joel.c@scene.community>
2018-04-14 23:48:39 +0100

    testing password reset2

--- django_dashboard/api/password_management.py
+++ django_dashboard/api/password_management.py
@@ -102,7 +102,7 @@ class PasswordManagementResource(ModelResource):
             pm.user = uob
             pm.save()
             bundle.data = {"message":"If your number exists in the system you will soon receive a message"}
-            send_mail('Smart Biogas Password Reset', 'Here is your reset code: '+ str(hid), 'hello@smartbiogas.net', ['chiara@creativenergie.co.uk','joel@creativenergie.co.uk'], fail_silently=False, )   
+            send_mail('Smart Biogas Password Reset', 'Here is your reset code: '+ str(hid), 'hello@smartbiogas.net', ['joel@creativenergie.co.uk'], fail_silently=False, )   
         return self.create_response(request, bundle)
         
         
